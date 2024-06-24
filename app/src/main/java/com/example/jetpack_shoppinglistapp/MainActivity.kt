ed2d4c1c9434ebeb3157a7fe94d183efd19e09f1
uthoai <uthoaimarma597@gmail.com>
2024-06-24 22:08:52 +0600

    AlertDialog, LazyColumn

--- app/src/main/java/com/example/jetpack_shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_shoppinglistapp/MainActivity.kt
@@ -3,12 +3,9 @@ package com.example.jetpack_shoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,25 +14,21 @@ import com.example.jetpack_shoppinglistapp.ui.theme.Jetpack_ShoppingListAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Jetpack_ShoppingListAppTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting() {
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Jetpack_ShoppingListAppTheme {
-
-    }
+    ShoppingListApp()
 }
